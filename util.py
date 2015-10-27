17e5eff203dadc935d3b93ddb1d28280909b0e94
kitchenknif <pavel.a.dmitriev@gmail.com>
2015-10-27 20:35:14 +0300

    started analytic quartic solution

---
+++ util.py
@@ -0,0 +1,20 @@
+import numpy
+
+
+def solve_quartic(a, b, c, d, e):
+
+    p = (8.*a*c - 3.*b**2) / (8.*a**2)
+    q = (b**3 - 4.*a*b*c + 8.*a**2*d) / (8.*a**3)
+
+    D0 = c**2 - 3*b*d + 12*a*e
+    D1 = 2*c**3 - 9*b*c*d + 27*b**2*e + 27*a*d**2 - 72*a*d**2 - 72*a*c*e
+
+    Q = numpy.power((D1 + numpy.sqrt(D1**2 - 4*D0**3))/2., 1./3.)
+    S = (1/2) * numpy.sqrt(-(2/3)*p + (1/(3*a))*(Q + D0/Q))
+
+    x1 = -b/(4.*a) + S + (1./2.)*numpy.sqrt(-4.*S**2 - 2*p + q/S)
+    x2 = -b/(4.*a) + S - (1./2.)*numpy.sqrt(-4.*S**2 - 2*p + q/S)
+    x3 = -b/(4.*a) + S + (1./2.)*numpy.sqrt(-4.*S**2 - 2*p - q/S)
+    x4 = -b/(4.*a) + S - (1./2.)*numpy.sqrt(-4.*S**2 - 2*p - q/S)
+
+    return x1, x2, x3, x4
