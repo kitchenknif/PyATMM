992f9c2e899e0852a6989e666254161777b64ec6
kitchenknif <pavel.a.dmitriev@gmail.com>
2015-11-02 01:19:23 +0300

    analytic solutions wip

--- tests/test_solve_quartic.py
+++ tests/test_solve_quartic.py
@@ -5,10 +5,10 @@ __author__ = 'p.dmitriev'
 
 class TestSolve_quartic(TestCase):
     def test_solve_quartic(self):
-        a = 1
-        b = 0
-        c = 0
-        d = 0
-        e = -1
+        a = 1.
+        b = 0.
+        c = 0.
+        d = 0.
+        e = -1.
         print(solve_quartic(a, b, c, d, e))
         self.fail()
--- util.py
+++ util.py
@@ -8,9 +8,17 @@ def solve_quartic(a, b, c, d, e):
 
     D0 = c**2 - 3*b*d + 12*a*e
     D1 = 2*c**3 - 9*b*c*d + 27*b**2*e + 27*a*d**2 - 72*a*d**2 - 72*a*c*e
+    D = (1./27.)*(4*D0**3 - D1**2)
 
-    Q = numpy.power((D1 + numpy.sqrt(D1**2 - 4*D0**3))/2., 1./3.)
-    S = (1/2) * numpy.sqrt(-(2/3)*p + (1/(3*a))*(Q + D0/Q))
+    if D != 0 and D0 == 0:
+        Q = numpy.power((D1 + numpy.sign(D1)*numpy.sqrt(D1**2 - 4*D0**3))/2., 1./3.)
+    else:
+        Q = numpy.power((D1 + numpy.sqrt(D1**2 - 4*D0**3))/2., 1./3.)
+
+    S = (1./2.) * numpy.sqrt(-(2./3.)*p + (1./(3.*a))*(Q + D0/Q))
+    if S == 0:
+        Q = -Q
+        S = (1/2) * numpy.sqrt(-(2/3)*p + (1/(3*a))*(Q + D0/Q))
 
     x1 = -b/(4.*a) + S + (1./2.)*numpy.sqrt(-4.*S**2 - 2*p + q/S)
     x2 = -b/(4.*a) + S - (1./2.)*numpy.sqrt(-4.*S**2 - 2*p + q/S)
