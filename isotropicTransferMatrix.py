4541bda4da4048a648fe336403c26027d6f8bffa
kitchenknif <pavel.a.dmitriev@gmail.com>
2016-01-20 15:05:46 +0300

    wip

--- isotropicTransferMatrix.py
+++ isotropicTransferMatrix.py
@@ -6,18 +6,20 @@ from transferMatrix import *
 
 def isotropic_polarizations(w, eps, kx, ky, kz):
 
-    if not numpy.isclose(kx, 0):
-        p_1 = [kz[0]/kx, 0, 1]
-        p_2 = [kz[1]/kx, 0, 1]
-    else:
-        p_1 = [1, 0, 0]
-        p_2 = [1, 0, 0]
-    if not numpy.isclose(ky, 0):
-        p_3 = [0, kz[2]/ky, 1]
-        p_4 = [0, kz[3]/ky, 1]
-    else:
-        p_3 = [0, 1, 0]
-        p_4 = [0, 1, 0]
+    #if not numpy.isclose(kx, 0):
+    p_1 = [numpy.cos(kx/kz[0]), 0, numpy.abs(numpy.sin(kx/kz[0]))]
+    p_2 = [numpy.cos(kx/kz[1]), 0, numpy.abs(numpy.sin(kx/kz[1]))]
+    #else:
+    #    p_1 = [1, 0, 0]
+    #    p_2 = [1, 0, 0]
+    #if not numpy.isclose(ky, 0):
+    #    p_3 = [0, kz[2]/ky, 1]
+    #    p_4 = [0, kz[3]/ky, 1]
+    #else:
+    #    p_3 = [0, 1, 0]
+    #    p_4 = [0, 1, 0]
+    p_3 = [0, numpy.cos(ky/kz[2]), numpy.abs(numpy.sin(ky/kz[2]))]
+    p_4 = [0, numpy.cos(ky/kz[3]), numpy.abs(numpy.sin(ky/kz[3]))]
     p = [p_1, p_2, p_3, p_4]
 
     p = [numpy.divide(pi, numpy.sqrt(numpy.dot(pi, pi))) for pi in p]
@@ -29,7 +31,7 @@ def build_isotropic_layer_matrix(eps, w, kx, ky, d):
     mu = 1.
     c = 299792458.  # m/c
 
-    mod_kz = numpy.sqrt((w/c)**2 - kx**2 - ky**2)*numpy.sqrt(eps)
+    mod_kz = numpy.sqrt(eps*(w/c)**2 - kx**2 - ky**2)
     gamma = [mod_kz, -mod_kz, mod_kz, -mod_kz]
     k = [numpy.asarray([kx, ky, g]) for g in gamma]
 
@@ -44,6 +46,8 @@ def build_isotropic_layer_matrix(eps, w, kx, ky, d):
     #
     # Build boundary transition matrix
     #
+    D_0 = build_vacuum_matrix(w, kx, ky)
+
     D = numpy.asarray(
         [
             [p[0][0], p[1][0], p[2][0], p[3][0]],
@@ -52,7 +56,6 @@ def build_isotropic_layer_matrix(eps, w, kx, ky, d):
             [q[0][0], q[1][0], q[2][0], q[3][0]]
         ], dtype=numpy.complex128
     )
-
     #
     # Build propagation matrix
     #
@@ -68,15 +71,15 @@ def build_isotropic_layer_matrix(eps, w, kx, ky, d):
     #
     # Multiply matricies
     #
-    print(D[0,0]*P[0,0])
-    LayerMatrix = numpy.dot(numpy.dot(D, P), numpy.linalg.inv(D))
+    LayerMatrix = numpy.linalg.inv(D_0)
+    LayerMatrix = numpy.dot(LayerMatrix, numpy.dot(numpy.dot(D, P), numpy.dot(numpy.linalg.inv(D), D_0)))
     return LayerMatrix
 
 def build_isotropic_bounding_layer_matrix(eps, w, kx, ky):
     mu = 1.
     c = 299792458.  # m/c
 
-    mod_kz = numpy.sqrt((w/c)**2 - kx**2 - ky**2)*numpy.sqrt(eps)
+    mod_kz = numpy.sqrt(eps*(w/c)**2 - kx**2 - ky**2)
     gamma = [mod_kz, -mod_kz, mod_kz, -mod_kz]
     k = [numpy.asarray([kx, ky, g]) for g in gamma]
 
@@ -85,6 +88,58 @@ def build_isotropic_bounding_layer_matrix(eps, w, kx, ky):
     #
 
     p = isotropic_polarizations(w, eps, kx, ky, gamma)
+    q = [(c/(w*mu))*numpy.cross(ki, pi) for ki, pi in zip(k, p)]
+
+    #
+    # Build boundary transition matrix
+    #
+    D_0 = build_vacuum_matrix(w, kx, ky)
+
+    D = numpy.asarray(
+        [
+            [p[0][0], p[1][0], p[2][0], p[3][0]],
+            [q[0][1], q[1][1], q[2][1], q[3][1]],
+            [p[0][1], p[1][1], p[2][1], p[3][1]],
+            [q[0][0], q[1][0], q[2][0], q[3][0]]
+        ], dtype=numpy.complex128
+    )
+    return numpy.dot(numpy.linalg.inv(D_0), D)
+
+def build_isotropic_propagation_matrix(eps, w, kx, ky, d):
+    mu = 1.
+    c = 299792458.  # m/c
+
+    mod_kz = numpy.sqrt(eps*(w/c)**2 - kx**2 - ky**2)
+    gamma = [mod_kz, -mod_kz, mod_kz, -mod_kz]
+    k = [numpy.asarray([kx, ky, g]) for g in gamma]
+
+    #
+    # Build propagation matrix
+    #
+    P = numpy.asarray(
+        [
+            [numpy.exp(-1j*gamma[0]*d), 0, 0, 0],
+            [0, numpy.exp(-1j*gamma[1]*d), 0, 0],
+            [0, 0, numpy.exp(-1j*gamma[2]*d), 0],
+            [0, 0, 0, numpy.exp(-1j*gamma[3]*d)]
+        ], dtype=numpy.complex128
+    )
+
+    return P
+
+def build_vacuum_matrix(w, kx, ky):
+    mu = 1.
+    c = 299792458.  # m/c
+
+    mod_kz = numpy.sqrt((w/c)**2 - kx**2 - ky**2)
+    gamma = [mod_kz, -mod_kz, mod_kz, -mod_kz]
+    k = [numpy.asarray([kx, ky, g]) for g in gamma]
+
+    #
+    # Build polarization vectors
+    #
+
+    p = isotropic_polarizations(w, 1, kx, ky, gamma)
     q = [(c/(w*mu))*numpy.cross(ki, pi) for ki, pi in zip(k, p)]
 
     #
--- test_isotropic.py
+++ test_isotropic.py
@@ -4,14 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from isotropicTransferMatrix import *
 from transferMatrix import solve_transfer_matrix
-import PyTMM.pytmm.transferMatrix as tm
+import pytmm.transferMatrix as tm
 import scipy.linalg
 
 c = 299792458  # m/c
 
 ran_w = np.linspace(50, 1200, 100, endpoint=False)
 ran_w = np.multiply(ran_w, 10 ** 12) # (1/(2*np.pi))*
-ran_l = np.divide(c, ran_w)
+ran_l = np.divide(c*(2*np.pi), ran_w)
 
 eps_1 = 2.25
 n_1 = np.sqrt(eps_1)
@@ -35,7 +35,8 @@ w = ran_w[0]
 
 
 refl0 = []
-refl = []
+refl_p = []
+refl_s = []
 for i in ran_l:
     # substrate layer (considered infinite, so only bounding layer needed)
     #b = tm.TransferMatrix.boundingLayer(1, n_1)
@@ -44,42 +45,48 @@ for i in ran_l:
     #refl0.append(np.abs(R**2))
 
     # antireflective layer layer "left" of substrate
-    b = tm.TransferMatrix.layer(n_1, d_nm, i*10**9)
+    B = tm.TransferMatrix.layer(n_1, d_nm, i*10**9, np.arcsin(kx*c/w), tm.Polarization.s)
+    C = tm.TransferMatrix.layer(n_1, d_nm, i*10**9, np.arcsin(kx*c/w), tm.Polarization.p)
     #a.appendRight(b)
 
-    B = scipy.linalg.block_diag(b.matrix, b.matrix)
-    r_ss, r_sp, r_ps, r_pp, t_ss, t_sp, t_ps, t_pp = solve_transfer_matrix(B)
-    refl.append(np.abs(r_pp**2))
+    M = scipy.linalg.block_diag(B.matrix, C.matrix)
+    r_ss, r_sp, r_ps, r_pp, t_ss, t_sp, t_ps, t_pp = solve_transfer_matrix(M)
+    refl_p.append(np.abs(r_pp**2))
+    refl_s.append(np.abs(r_ss**2))
 
     #R, T = tm.solvePropagation(b)
     #refl.append(np.abs(R**2))
 
-a_refl = []
-a_refl0 = []
+a_refl_p = []
+a_refl_s = []
 for w in ran_w:
     #kx = c/(np.sqrt(2)*w)
     kx = 0
-    D = build_isotropic_layer_matrix(eps_1, w, kx, ky, d_m)
     #D = build_isotropic_bounding_layer_matrix(eps_1, w, kx, ky)
+    D = build_isotropic_layer_matrix(eps_1, w, kx, ky, d_m)
+    #D = build_isotropic_propagation_matrix(eps_1, w, kx, ky, d_m)
 
     #r_ss, r_sp, r_ps, r_pp, t_ss, t_sp, t_ps, t_pp = solve_transfer_matrix(numpy.dot(D_b, D))
     #a_refl.append(np.abs(r_pp**2))
 
     r_ss, r_sp, r_ps, r_pp, t_ss, t_sp, t_ps, t_pp = solve_transfer_matrix(D)
-    a_refl0.append(np.abs(r_pp**2))
+    a_refl_p.append(np.abs(r_pp**2))
+    a_refl_s.append(np.abs(r_ss**2))
 
 
 #PyATMM
-plt.plot(ran_l*10**9, a_refl0)
-#plt.plot(ran_l*10**9, a_refl)
+plt.plot(ran_l*10**9, a_refl_p)
+plt.plot(ran_l*10**9, a_refl_s)
+#plt.plot(ran_l*10**9, sum)
 
 #PyTMM
 #plt.plot(ran_l*10**9, refl0, 'o')
-plt.plot(ran_l*10**9, refl, 'o')
+plt.plot(ran_l*10**9, refl_p, 'o')
+plt.plot(ran_l*10**9, refl_s, 'o')
 
 
 plt.xlabel("Wavelength, nm")
 plt.ylabel("Reflectance")
 plt.title("Reflectance of ideal single-layer antireflective coating")
-plt.legend(['ATMM', 'TMM'], loc='best')
+plt.legend(['PP', 'SS', 'P', 'S'], loc='best')
 plt.show(block=True)
